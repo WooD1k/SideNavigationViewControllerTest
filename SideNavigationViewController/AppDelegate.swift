9645b9b65227425ec2716e75e924418953932e34
Alexey Chulochnikov <chulochnikov@gmail.com>
2015-11-30 00:52:43 +0200

    - initial commit

--- SideNavigationViewController/AppDelegate.swift
+++ SideNavigationViewController/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window!.rootViewController = SideNavigationViewController(mainViewController: ViewController(), sideViewController: SideMenuViewController())
+        window!.makeKeyAndVisible()
+        
         return true
     }
 
